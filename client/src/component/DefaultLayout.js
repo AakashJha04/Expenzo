80f710cbad9fc36503ff161f75b7321ab6175dac
AakashJha04 <aakashthedeveloper04@gmail.com>
2025-06-20 23:13:01 +0530

    dintana

--- client/src/component/DefaultLayout.js
+++ client/src/component/DefaultLayout.js
@@ -1,23 +1,35 @@
 import React from "react";
-import '../resources/default-layout.css'
+import { Button } from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
+import "../resources/default-layout.css";
 
 function DefaultLayout(props) {
   const user = JSON.parse(localStorage.getItem("expenzo-user"));
+
+  const handleLogout = () => {
+    localStorage.removeItem("expenzo-user");
+    window.location.href = "/login"; // redirect to login
+  };
+
   return (
     <div className="layout">
       <div className="header d-flex justify-content-between align-items-center">
         <div>
           <h1 className="logo">Expenzo</h1>
         </div>
-        <div>
-            <h1 className="username">
-                {user.name}
-            </h1>
+        <div className="d-flex align-items-center gap-2">
+          <h1 className="username mb-0">{user.name}</h1>
+          <Button
+            type="primary"
+            danger
+            shape="circle"
+            icon={<LogoutOutlined />}
+            onClick={handleLogout}
+          />
         </div>
       </div>
 
       <div className="content">{props.children}</div>
-
     </div>
   );
 }
