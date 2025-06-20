22cbb1b4286699c3c2dd75af0de2e00c0ccc339a
AakashJha04 <aakashthedeveloper04@gmail.com>
2025-06-20 10:38:15 +0530

    Added frontend setup and implemented layout

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DefaultLayout from '../component/DefaultLayout'
+
+function Home() {
+  return (
+    <DefaultLayout> THIS IS HOME PAGE</DefaultLayout>
+  )
+}
+
+export default Home
---
+++ client/src/pages/Test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DefaultLayout from '../component/DefaultLayout'
+
+function Test() {
+  return (
+    <DefaultLayout> THIS IS TEST PAGE</DefaultLayout>
+  )
+}
+
+export default Test
