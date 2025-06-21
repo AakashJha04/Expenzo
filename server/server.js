f237f579ebdab96b9a4dc26dd2d7937c4d01ab15
AakashJha04 <aakashthedeveloper04@gmail.com>
2025-06-21 15:52:18 +0530

    Revert "all changes for production"
    This reverts commit c919724427fbe5187bdcc07ff73549a589ccc293.

--- server/server.js
+++ server/server.js
@@ -1,23 +1,22 @@
 const express  = require('express');
 const dbConnect = require('./dbConnect');
 const app = express();
-require('dotenv').config();
 app.use(express.json());
-const path = require('path');
+
 const userRoute = require('./routes/userRoute'); 
 const transactionRoute = require('./routes/transactionRoute');
  
 app.use('/api/users/', userRoute);
 app.use('/api/transaction', transactionRoute);
 
-const PORT = process.env.PORT || 4000
+const PORT = 4000
 
-if(process.env.NODE_ENV === 'production'){
-    app.use('/', express.static('client/build'));
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-    })
-}
+
+app.get('/', (req,res) => {
+    res.send("HELLO WORLD JI")
+});
+
+
 
 app.listen(PORT, () => console.log(`SERVER IS LISTENING AT ${PORT}`));
 
