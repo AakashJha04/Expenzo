c919724427fbe5187bdcc07ff73549a589ccc293
AakashJha04 <aakashthedeveloper04@gmail.com>
2025-06-21 15:16:37 +0530

    all changes for production

--- server/server.js
+++ server/server.js
@@ -1,22 +1,23 @@
 const express  = require('express');
 const dbConnect = require('./dbConnect');
 const app = express();
+require('dotenv').config();
 app.use(express.json());
-
+const path = require('path');
 const userRoute = require('./routes/userRoute'); 
 const transactionRoute = require('./routes/transactionRoute');
  
 app.use('/api/users/', userRoute);
 app.use('/api/transaction', transactionRoute);
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
-
-app.get('/', (req,res) => {
-    res.send("HELLO WORLD JI")
-});
-
-
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static('client/build'));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
 
 app.listen(PORT, () => console.log(`SERVER IS LISTENING AT ${PORT}`));
 
